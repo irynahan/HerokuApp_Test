414d81d06247704470755783ce82564dbefde873
Iryna Hanuliak <hanuliak.iryna@gmail.com>
2023-08-27 21:17:53 +0200

    Test base

--- src/test/java/com/herokuapp/theinternet/base/BaseTest.java
+++ src/test/java/com/herokuapp/theinternet/base/BaseTest.java
@@ -1,34 +1,55 @@
 package com.herokuapp.theinternet.base;
 
 import org.openqa.selenium.WebDriver;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Optional;
-import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.*;
+
+import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
     protected WebDriver driver;
 
-
-
     @Parameters({"browser"})
     @BeforeMethod(alwaysRun = true)
-    public void setUp(@Optional("chrome") String browser){
+    public void setUp(@Optional("chrome") String browser) {
 
         // create driver
         System.out.println("Create driver: " + browser);
 
-        switch (browser):
-        case "chrome":
-            System.setProperty("webdriver.chrome.driver", "");
+        switch (browser) {
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", "C:\\projects\\Tools\\chrome-driver\\chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", "C:\\projects\\Tools\\geckodriver\\geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+
+            default:
+                System.out.println("Do not know how to start: " + browser + ", starting chrome.");
+                System.setProperty("webdriver.chrome.driver", "C:\\projects\\Tools\\chrome-driver");
+                driver = new ChromeDriver();
+                break;
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
     }
 
-    @Test
-    public void printTest() {
-        System.out.println("Method is executed");
-    }
+    @AfterMethod
+    public void tearDown() {
+        // close browser
+        System.out.println("Close driver");
+        driver.quit();
 
+    }
 
 }
+
+
+
+
