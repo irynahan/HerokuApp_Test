a16dec674e7f31047db0abd6aecd0a02631de7bb
Iryna Hanuliak <hanuliak.iryna@gmail.com>
2023-08-31 16:21:34 +0200

    Swich window test

---
+++ src/test/java/com/herokuapp/theinternet/windowstests/WindowsTests.java
@@ -0,0 +1,39 @@
+package com.herokuapp.theinternet.windowstests;
+
+import com.beust.jcommander.Parameters;
+import com.herokuapp.theinternet.base.TestUtilities;
+import com.herokuapp.theinternet.page.NewWindowPage;
+import com.herokuapp.theinternet.page.WelcomePageObject;
+import com.herokuapp.theinternet.page.WindowsPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.sql.SQLOutput;
+
+public class WindowsTests extends TestUtilities {
+
+    @Test
+    public void newWindowTest() {
+        log.info("Starting newWindowTest");
+
+        // open main page
+        WelcomePageObject welcomePage = new WelcomePageObject(driver, log);
+        welcomePage.openPage();
+
+        // Click on MultipleWindows link
+        WindowsPage windowsPage = welcomePage.clickMultipleWindowsLink();
+
+        // Click 'Click Here' link to open new window
+        windowsPage.openNewWindow();
+        //sleep(1000);
+
+        // Find and switch to new window page
+        NewWindowPage newWindowPage = windowsPage.switchToNewWindowPage();
+
+        String pageSource = newWindowPage.getCurrentPageSource();
+
+        // Verification that new page contains expected text in source
+        Assert.assertTrue(pageSource.contains("New Window"), "New page source doesn't contain expected text");
+    }
+
+}
