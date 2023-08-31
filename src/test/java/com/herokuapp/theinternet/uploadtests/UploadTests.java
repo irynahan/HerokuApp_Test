e75ae1bfdd4cf7bfd3a3479836c39f707fd676a2
Iryna Hanuliak <hanuliak.iryna@gmail.com>
2023-08-31 18:22:36 +0200

    Uploading file

---
+++ src/test/java/com/herokuapp/theinternet/uploadtests/UploadTests.java
@@ -0,0 +1,34 @@
+package com.herokuapp.theinternet.uploadtests;
+
+import com.herokuapp.theinternet.base.TestUtilities;
+import com.herokuapp.theinternet.page.FileUploaderPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UploadTests extends TestUtilities {
+
+    @Test
+    public void imageUploadTest() {
+        log.info("Starting imageUploadTest");
+
+        // open File Uploader Page
+        FileUploaderPage fileUploaderPage = new FileUploaderPage(driver, log);
+        fileUploaderPage.openPage();
+
+        // Select file
+        String fileName = "IMG_2240.jpg";
+        fileUploaderPage.selectFile(fileName);
+
+        // Push upload button
+        fileUploaderPage.pushUploadButton();
+
+        // Get uploaded files
+        String fileNames = fileUploaderPage.getUploadedFilesNames();
+
+        // Verify selected file uploaded
+        Assert.assertTrue(fileNames.contains(fileName),
+                "Our file (" + fileName + ") is not one of the uploaded (" + fileNames + ")");
+    }
+
+
+}
