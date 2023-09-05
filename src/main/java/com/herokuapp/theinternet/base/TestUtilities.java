36a2d15ef14df1029ead6aee145d4cee9b6eaef8
Iryna Hanuliak <hanuliak.iryna@gmail.com>
2023-09-05 22:06:25 +0200

    static data provider (array Objects)

--- src/main/java/com/herokuapp/theinternet/base/TestUtilities.java
+++ src/main/java/com/herokuapp/theinternet/base/TestUtilities.java
@@ -1,14 +1,26 @@
 package com.herokuapp.theinternet.base;
 
+import org.testng.annotations.DataProvider;
+
 public class TestUtilities extends BaseTest{
 
-    // STATIC SLEEP
+    // sleep for test debugging
     protected void sleep(long millis) {
         try {
             Thread.sleep(millis);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    // static data provider for delivery a list of files as array of arrays object, to be downloaded
+    @DataProvider(name="filesToDownload")
+    protected static Object[][] files() {
+        return new Object[][] {
+                {1,"index.txt"},
+                {2,"logo.txt"},
+                {3,"text.txt"}
+        };
     }
 
 }
--- src/test/java/com/herokuapp/theinternet/uploadtests/UploadTests.java
+++ src/test/java/com/herokuapp/theinternet/uploadtests/UploadTests.java
@@ -8,8 +8,8 @@ import org.testng.annotations.Test;
 public class UploadTests extends TestUtilities {
 
     @Test
-    public void imageUploadTest() {
-        log.info("Starting imageUploadTest");
+    public void textUploadTest() {
+        log.info("Starting textUploadTest");
 
         // open File Uploader Page
         FileUploaderPage fileUploaderPage = new FileUploaderPage(driver, log);
@@ -29,6 +29,30 @@ public class UploadTests extends TestUtilities {
         Assert.assertTrue(fileNames.contains(fileName),
                 "Our file (" + fileName + ") is not one of the uploaded (" + fileNames + ")");
     }
+
+    // file upload tests with static data provider
+    @Test (dataProvider = "filesToDownload")
+    public void fileUploadTest( int testIndex, String fileName) {
+        log.info("Starting fileUploadTest " + testIndex + " " + fileName );
+
+        // open File Uploader Page
+        FileUploaderPage fileUploaderPage = new FileUploaderPage(driver, log);
+        fileUploaderPage.openPage();
+
+        // Select file
+        fileUploaderPage.selectFile(fileName);
+
+        // Push upload button
+        fileUploaderPage.pushUploadButton();
+
+        // Get uploaded files
+        String fileNames = fileUploaderPage.getUploadedFilesNames();
+
+        // Verify selected file uploaded
+        Assert.assertTrue(fileNames.contains(fileName),
+                "Our file (" + fileName + ") is not one of the uploaded (" + fileNames + ")");
+    }
+
 
 
 }
