8f9ee6baa7aa40f1268a34876ed1c883b9c404bb
Iryna Hanuliak <hanuliak.iryna@gmail.com>
2023-09-05 13:20:31 +0200

    Horizontal Slider

---
+++ src/main/java/com/herokuapp/theinternet/page/HorizontalSliderPage.java
@@ -0,0 +1,12 @@
+package com.herokuapp.theinternet.page;
+
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.WebDriver;
+
+public class HorizontalSliderPage extends BasePageObject{
+
+    public HorizontalSliderPage(WebDriver driver, Logger log) {
+        super(driver, log);
+    }
+    
+}
---
+++ src/test/java/com/herokuapp/theinternet/horizontalslidertests/HorizontalSliderTests.java
@@ -0,0 +1,8 @@
+package com.herokuapp.theinternet.horizontalslidertests;
+
+import com.herokuapp.theinternet.base.TestUtilities;
+
+public class HorizontalSliderTests extends TestUtilities {
+
+
+}
