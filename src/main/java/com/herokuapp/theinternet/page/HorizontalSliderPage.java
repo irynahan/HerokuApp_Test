4f21d366d1b99746189be92970559f42c86cf3b2
Iryna Hanuliak <hanuliak.iryna@gmail.com>
2023-09-05 14:31:43 +0200

    slider test

--- src/main/java/com/herokuapp/theinternet/page/HorizontalSliderPage.java
+++ src/main/java/com/herokuapp/theinternet/page/HorizontalSliderPage.java
@@ -1,6 +1,7 @@
 package com.herokuapp.theinternet.page;
 
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HorizontalSliderPage extends BasePageObject{
@@ -8,5 +9,50 @@ public class HorizontalSliderPage extends BasePageObject{
     public HorizontalSliderPage(WebDriver driver, Logger log) {
         super(driver, log);
     }
-    
+
+    private By rangeLocator = By.id("range");
+    private By sliderLocator = By.tagName("input");
+
+    private String  pageUrl = "http://the-internet.herokuapp.com/horizontal_slider";
+
+    public void openPage() {
+        log.info("Opening page: " + pageUrl);
+        openUrl(pageUrl);
+        log.info("Page opened!");
+    }
+
+    /** Move slider to specified value */
+    public void setSliderTo(String value) {
+        log.info("Moving slider to " + value);
+
+        // Move slider normal method
+        // Find the xOffset using given value
+        // int width = find(sliderLocator).getSize().getWidth();
+        // double percent = Double.parseDouble(value) / 5;
+        // int xOffset = (int) (width * percent);
+
+        // Actions action = new Actions(driver);
+        // action.dragAndDropBy(find(sliderLocator), xOffset, 0).build().perform();
+
+        // Workaround method
+        // Calculate number of steps
+        int steps = (int) (Double.parseDouble(value) / 0.5);
+
+        // Perform steps
+        pressKey(sliderLocator, Keys.ENTER);
+        for (int i = 0; i < steps; i++) {
+            pressKey(sliderLocator, Keys.ARROW_RIGHT);
+        }
+    }
+
+    /** Getting slider value */
+    public String getSliderValue() {
+        String value  = find(rangeLocator).getText();
+        log.info("Slider value is " + value);
+        return value;
+    }
+
+
+
+
 }
--- src/test/java/com/herokuapp/theinternet/horizontalslidertests/HorizontalSliderTests.java
+++ src/test/java/com/herokuapp/theinternet/horizontalslidertests/HorizontalSliderTests.java
@@ -1,8 +1,31 @@
 package com.herokuapp.theinternet.horizontalslidertests;
 
 import com.herokuapp.theinternet.base.TestUtilities;
+import com.herokuapp.theinternet.page.HorizontalSliderPage;
+import org.testng.Assert;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
 
 public class HorizontalSliderTests extends TestUtilities {
 
+    @Parameters({"value"})
+    @Test
+    public void sliderTest(@Optional("1") String value) {
 
+        // Open HorizontalSliderPage
+        HorizontalSliderPage horizontalSliderPage = new HorizontalSliderPage(driver, log);
+        horizontalSliderPage.openPage();
+
+        // set slider value
+        sleep(2000);
+        horizontalSliderPage.setSliderTo(value);
+        sleep(2000);
+
+        // Verify slider value
+        String sliderValue = horizontalSliderPage.getSliderValue();
+        Assert.assertTrue(sliderValue.equals(value), "Range is not correct. It is: " + sliderValue);
+
+    }
+    
 }
