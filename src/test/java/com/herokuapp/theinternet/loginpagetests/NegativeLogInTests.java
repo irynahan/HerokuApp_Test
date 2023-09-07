c931328d15e78a34234c22a0276fb8c7b9316ede
Iryna Hanuliak <hanuliak.iryna@gmail.com>
2023-09-07 14:16:22 +0200

    Data provider with csv file

--- src/test/java/com/herokuapp/theinternet/loginpagetests/NegativeLogInTests.java
+++ src/test/java/com/herokuapp/theinternet/loginpagetests/NegativeLogInTests.java
@@ -1,19 +1,22 @@
 package com.herokuapp.theinternet.loginpagetests;
 
 import com.herokuapp.theinternet.base.BaseTest;
+import com.herokuapp.theinternet.base.CsvDataProviders;
 import com.herokuapp.theinternet.page.LoginPage;
 import com.herokuapp.theinternet.page.WelcomePage;
 import org.testng.Assert;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
+import java.util.Map;
+
 public class NegativeLogInTests extends BaseTest {
 
     @Parameters ({"username", "password", "expectedMessage"})
-    @Test
-    public void negativeLogInTest(String username, String password, String expectedMessage) {
+    @Test (priority = 1)
+    public void negativeLogInTestParam(String username, String password, String expectedMessage) {
 
-        log.info("Starting negative logIn test");
+        log.info("Starting negativeLogInTestParam");
 
         WelcomePage welcomePage = new WelcomePage(driver, log);
         welcomePage.openPage();
@@ -35,4 +38,40 @@ public class NegativeLogInTests extends BaseTest {
         Assert.assertTrue(actualLogInErrorMessage.contains(expectedMessage), "Actual error message does not meet expected message. /n Actual: " + actualLogInErrorMessage + "/n Expected:"  + expectedMessage);
 
     }
+
+    // login test with data csv provider
+
+    @Test (enabled = true, priority = 2, dataProvider = "csvReader", dataProviderClass = CsvDataProviders.class)
+    public void negativeLogInTest(Map<String, String> testData) {
+        // Data
+        String no = testData.get("no");
+        String username  = testData.get("username");
+        String password = testData.get("password");
+        String expectedErrorMessage = testData.get("expectedMessage");
+        String description = testData.get("description");
+
+        log.info("Starting negativeLogInTest #" + no + " for " + description);
+
+        // open main page
+        WelcomePage welcomePage = new WelcomePage(driver, log);
+        welcomePage.openPage();
+
+        // Click on Form Authentication link
+        LoginPage loginPage = welcomePage.clickFormAuthenticationLink();
+
+        // execute log in
+        loginPage.negativeLogIn(username, password);
+
+        // wait for error message
+        loginPage.waitForErrorMessage();
+        String actualLogInErrorMessage = loginPage.getErrorMessageText();
+
+
+        // Verifications
+        // verify error message
+
+        Assert.assertTrue(actualLogInErrorMessage.contains(expectedErrorMessage), "Actual error message does not meet expected message. /n Actual: " + actualLogInErrorMessage + "/n Expected:"  + expectedErrorMessage);
+
+    }
+
 }
