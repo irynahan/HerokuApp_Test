857a6483b608d819dd151e70a731618806727f3a
Iryna Hanuliak <hanuliak.iryna@gmail.com>
2023-09-10 12:30:37 +0200

    Add cookie and ser cookie value

--- src/test/java/com/herokuapp/theinternet/loginpagetests/PositiveLogInTests.java
+++ src/test/java/com/herokuapp/theinternet/loginpagetests/PositiveLogInTests.java
@@ -5,6 +5,7 @@ import com.herokuapp.theinternet.base.TestUtilities;
 import com.herokuapp.theinternet.page.LoginPage;
 import com.herokuapp.theinternet.page.SecureAreaPage;
 import com.herokuapp.theinternet.page.WelcomePage;
+import org.openqa.selenium.Cookie;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -19,9 +20,20 @@ public class PositiveLogInTests extends TestUtilities {
         // Click on Form Authentication link
         LoginPage loginPage = welcomePage.clickFormAuthenticationLink();
 
+        // Add new cookie
+        Cookie ck = new Cookie("username", "tomsmith", "the-internet.herokuapp.com", "/", null);
+        loginPage.setCookie(ck);
+
         // execute log in
         SecureAreaPage secureAreaPage =  loginPage.logIn("tomsmith", "SuperSecretPassword!");
         takeScreenshot("LoginAndPassword");
+
+        // Get cookies
+        String userName = secureAreaPage.getCookie("username");
+        log.info("Cookie username: " + userName);
+        String sessionRack = secureAreaPage.getCookie("rack.session");
+        log.info("Session cookie: " + sessionRack);
+
 
         // Verifications
         // New page url is expected
